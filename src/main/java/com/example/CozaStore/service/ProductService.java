03d9de17f63b2c581c35384e486e481d78b44106
Leo Hoang <hoangvm95@gmail.com>
2023-06-07 18:46:15 +0700

    feature/addproduct

--- src/main/java/com/example/CozaStore/service/ProductService.java
+++ src/main/java/com/example/CozaStore/service/ProductService.java
@@ -1,6 +1,10 @@
 package com.example.CozaStore.service;
 
+import com.example.CozaStore.entity.CategoryEntity;
+import com.example.CozaStore.entity.ColorEntity;
 import com.example.CozaStore.entity.ProductEntity;
+import com.example.CozaStore.entity.SizeEntity;
+import com.example.CozaStore.payload.request.ProductRequest;
 import com.example.CozaStore.payload.response.ProductResponse;
 import com.example.CozaStore.repository.ProductRepository;
 import com.example.CozaStore.service.imp.IProductService;
@@ -30,5 +34,35 @@ public class ProductService implements IProductService {
             productResponseList.add(productResponse);
         }
         return productResponseList;
+    }
+
+    @Override
+    public boolean addProduct(ProductRequest productRequest) {
+        try {
+            ProductEntity productEntity = new ProductEntity();
+            productEntity.setName(productRequest.getName());
+            productEntity.setPrice(productRequest.getPrice());
+            productEntity.setDescription(productRequest.getDescription());
+            productEntity.setQuantity(productRequest.getQuantity());
+
+            SizeEntity sizeEntity = new SizeEntity();
+            sizeEntity.setId(productRequest.getSizeId());
+
+            ColorEntity colorEntity = new ColorEntity();
+            colorEntity.setId(productRequest.getColorId());
+
+            CategoryEntity categoryEntity = new CategoryEntity();
+            categoryEntity.setId(productRequest.getCategoryId());
+
+            productEntity.setColor(colorEntity);
+            productEntity.setSize(sizeEntity);
+            productEntity.setCategory(categoryEntity);
+
+            productRepository.save(productEntity);
+            return true;
+        } catch (Exception e) {
+
+            return false;
+        }
     }
 }
